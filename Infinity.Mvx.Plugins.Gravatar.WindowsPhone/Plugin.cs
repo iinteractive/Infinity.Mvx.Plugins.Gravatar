ea44bbbc7b10537b534dcda96a73d79a1945b058
Sean Sparkman <sean.sparkman@outlook.com>
2014-01-22 23:44:33 -0600

    added windows phone support

---
+++ Infinity.Mvx.Plugins.Gravatar.WindowsPhone/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore.Plugins;
+
+namespace Infinity.Mvx.Plugins.Gravatar.WindowsPhone
+{
+    public class Plugin
+        : IMvxPlugin
+    {
+        public void Load()
+        {
+            Cirrious.CrossCore.Mvx.RegisterType<IMvxGravatarTask, MvxGravatarTask>();
+        }
+    }
+}
