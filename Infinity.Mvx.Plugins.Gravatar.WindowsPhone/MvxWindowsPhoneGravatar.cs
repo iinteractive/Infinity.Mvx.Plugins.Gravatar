8c19e0aa72513ecd3578d8aaaadec1b1f154bb93
Sean Sparkman <sean.sparkman@outlook.com>
2014-02-14 15:02:11 -0600

    fixed windows phone hash

---
+++ Infinity.Mvx.Plugins.Gravatar.WindowsPhone/MvxWindowsPhoneGravatar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infinity.Mvx.Plugins.Gravatar.WindowsPhone
+{
+    public class MvxWindowsPhoneGravatar
+        : IMvxGravatarTask
+    {
+        private static readonly Func<byte[], byte[]> Md5;
+
+        static MvxWindowsPhoneGravatar()
+        {
+            Md5 = MD5Core.GetHash;
+        }
+
+        public Uri CreateGravatarUri(string email, int size)
+        {
+            return new Uri(CreateGravatarUrl(email, size));
+        }
+
+        public string CreateGravatarUrl(string email, int size)
+        {
+            var hashString = Md5(Encoding.UTF8.GetBytes(email.Trim()));
+
+            return string.Format("http://www.gravatar.com/avatar/{0}.jpg?s={1}&d=mm", string.Join("", hashString.Select(s => s.ToString("x2"))), size);
+        }
+    }
+}
--- Infinity.Mvx.Plugins.Gravatar.WindowsPhone/Plugin.cs
+++ Infinity.Mvx.Plugins.Gravatar.WindowsPhone/Plugin.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Cirrious.CrossCore.Plugins;
+﻿using Cirrious.CrossCore.Plugins;
 
 namespace Infinity.Mvx.Plugins.Gravatar.WindowsPhone
 {
@@ -12,7 +7,7 @@ namespace Infinity.Mvx.Plugins.Gravatar.WindowsPhone
     {
         public void Load()
         {
-            Cirrious.CrossCore.Mvx.RegisterType<IMvxGravatarTask, MvxGravatarTask>();
+            Cirrious.CrossCore.Mvx.RegisterSingleton<IMvxGravatarTask>(new MvxWindowsPhoneGravatar());
         }
     }
 }
